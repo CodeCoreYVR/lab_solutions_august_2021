e963590b15b7011ae789b5e9ee57483cf480379e
Mao Li <anrenyil@gmail.com>
2021-09-08 21:42:58 -0700

    fix flatten

--- js_high_order_function/reverse_engineer_flatten.js
+++ js_high_order_function/reverse_engineer_flatten.js
@@ -1,18 +1,19 @@
-// flatten([ [ 2, 3 ], [8, [5, 9], [4, 5]], 10]); // returns [ 2, 3, 8, 5, 9, 4, 5, 10 ]
-function flatten(arr, result) {
-    // loop each element 
-    // 1. check if the type of this element is array or not
-    // 2. if it is not an array, => push this element to the result
-    // 3. if it is an array
-    // loop this element => repeat steps 1,2,3
-    arr.forEach(element => {
-        if (Array.isArray(element)) {// [a,s]
-            flatten(element, result);
+function flatten(arr) {
+    const result = [];
+    arr.forEach(item => {
+        if (Array.isArray(item)) {
+            const temp = flatten(item);
+            temp.map(val => {
+                result.push(val);
+            })
+            // result.push(...flatten(item));
         } else {
-            result.push(element);
+            result.push(item);
         }
     });
     return result;
 }
 
-console.log(flatten([[2, 3], [8, [5, 9], [4, 5]], 10], []));
+console.log(flatten([1, 2, [3, [4, 5]]])); // returns [ 1, 2, 3, 4, 5 ]
+console.log(flatten(['a', ['b', ['c']]])); // returns [ 'a', 'b', 'c' ]
+console.log(flatten([[2, 3], [8, [5, 9], [4, 5]], 10])); // returns [ 2, 3, 8, 5, 9, 4, 5, 10 ]
