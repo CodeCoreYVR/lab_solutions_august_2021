f50159f08caa1c890f2f5bf5ffc7500ae5fed89f
Mao Li <anrenyil@gmail.com>
2021-10-27 16:49:14 -0700

    jquery lab3

--- js_event_propagation_and_jquery/jquery_lab/main.js
+++ js_event_propagation_and_jquery/jquery_lab/main.js
@@ -49,4 +49,28 @@ $('document').ready(() => {
     //#endregion
 
 
+    //#region lab 3
+    $(document).keydown(function (event) {
+        if (event.key === "g") {
+            $('.grey').toggle();
+        }
+        if (event.keyCode === 32) {
+            $("#green-container").append('<div class="small blue circle shape"></div>');
+        }
+    });
+
+    // Make the Form Message show the number of characters remaining
+    // (14 characters maximum) as you type in the text input. (e.g. "3 characters remaining").
+
+    $('#form-1 > input[type=text]').on('input', function () {
+        let remaining = 14 - $(this).val().length;
+        $('#form-message').text(`${remaining} characters remaining`);
+    });
+
+    // actually add the form validation, if you wish
+
+    $('#form-1 > input[type=text]').attr('maxlength', 14);
+    //#endregion
+
+
 });
