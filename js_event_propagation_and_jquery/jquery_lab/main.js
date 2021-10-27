7dbbf913c5873675645df0e792bd3c01b0e70d78
Mao Li <anrenyil@gmail.com>
2021-10-27 16:55:27 -0700

    jquery lab4

--- js_event_propagation_and_jquery/jquery_lab/main.js
+++ js_event_propagation_and_jquery/jquery_lab/main.js
@@ -25,13 +25,13 @@ $('document').ready(() => {
     $('tbody').prepend('<tr><td>0</td><td>-</td></tr>');
 
     // When the form's submit button is clicked, append the text input's current value to the form message.
-    $('#form-1').submit(function () {
-        $('#form-message').append(
-            $(this)
-                .find('input[type=text]')
-                .val()
-        );
-    });
+    // $('#form-1').on('submit', (function () {
+    //     $('#form-message').append(
+    //         $(this)
+    //             .find('input[type=text]')
+    //             .val()
+    //     );
+    // }));
 
     $('#button-1').on('click', () => {
         $("#green-container").toggle();
@@ -62,15 +62,40 @@ $('document').ready(() => {
     // Make the Form Message show the number of characters remaining
     // (14 characters maximum) as you type in the text input. (e.g. "3 characters remaining").
 
-    $('#form-1 > input[type=text]').on('input', function () {
-        let remaining = 14 - $(this).val().length;
-        $('#form-message').text(`${remaining} characters remaining`);
-    });
+    // $('#form-1 > input[type=text]').on('input', function () {
+    //     let remaining = 14 - $(this).val().length;
+    //     $('#form-message').text(`${remaining} characters remaining`);
+    // });
 
     // actually add the form validation, if you wish
 
     $('#form-1 > input[type=text]').attr('maxlength', 14);
     //#endregion
 
+    //region lab 4
 
+    $('#form-1').submit(function (e) {
+        const color = $('#form-1 > input[type=text]').val();
+
+        // If they enter an invalid color show them an alert message telling them,
+        // "Entered color is not a valid option!"
+        const validColors = ['red', 'blue', 'black', 'grey'];
+
+        if (!validColors.includes(color)) {
+            alert(`Entered colour, "${color}", is not a valid option!`);
+        } else {
+            // All shapes matching the given color should be removed.
+            $(`.shape.${color}`).remove();
+        }
+
+        // Clear the input field.
+        this.reset();
+        /* 
+        This really clears the entire form, but since the form only has one input,
+        it is equivalent.
+        */
+        // or
+        // $('#form-1 > input[type=text]').val('');
+    });
+    //endregion
 });
