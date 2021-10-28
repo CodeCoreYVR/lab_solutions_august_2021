bab7173e36b70b16eba90ca50ec321191d81ff2e
Mao Li <anrenyil@gmail.com>
2021-10-28 16:06:51 -0700

    rails api lab 1

---
+++ rails_basics/amazon_cohort42/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ProductsController < Api::ApplicationController
+    def index
+        products = Product.order(created_at: :desc)
+        render(json: products)
+    end
+    
+end
