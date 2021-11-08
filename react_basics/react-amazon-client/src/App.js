2a0cb92eff34718e23510103be44c0b3d913c933
Mao Li <anrenyil@gmail.com>
2021-11-08 15:17:05 -0800

    react basics lab 2

--- react_basics/react-amazon-client/src/App.js
+++ react_basics/react-amazon-client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import ProductDetails from './components/ProductDetails';
+import ReviewDetails from './components/ReviewDetails'
 
 function App() {
   return (
     <div className="App">
-
+      <ProductDetails
+        title="Phone"
+        description="This is a phone"
+        price={200}
+        created_at={new Date()}
+        seller={{ full_name: "Admin" }}
+      />
+      <ReviewDetails
+        rating={4}
+        body="good good"
+        created_at={new Date()}
+        full_name="Test User"
+      />
     </div>
   );
 }
---
+++ react_basics/react-amazon-client/src/components/ReviewDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ReviewDetails(props) {
+    return (
+        <p>
+            <strong>Rating: {props.rating}</strong>
+            <br />
+            {props.body}
+            <br />
+            Created at: {props.created_at.toString()}
+            <br />
+            Created by: {props.full_name}
+        </p>
+    )
+}
