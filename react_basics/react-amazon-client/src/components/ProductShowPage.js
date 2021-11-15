567177c857270848b354bf83a6e840b4f82fce07
Mao Li <anrenyil@gmail.com>
2021-11-15 15:01:56 -0800

    react hooks lab 2

--- react_basics/react-amazon-client/src/components/ProductShowPage.js
+++ react_basics/react-amazon-client/src/components/ProductShowPage.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 import ProductDetails from './ProductDetails';
 import ReviewList from './ReviewList';
-import { Product } from '../request'
+import { Product } from '../request';
+import NewReviewForm from './NewReviewForm'
 
 
 export default class ProductShowPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            review: {
+                body: '',
+                rating: 1
+            }
+        };
         this.deleteReview = this.deleteReview.bind(this);
+        this.handleReviewForm = this.handleReviewForm.bind(this);
+        this.handleAddNewReview = this.handleAddNewReview.bind(this);
     }
     componentDidMount() {
         const pid = this.props.match.params.id;
         Product.one(pid).then(data => {
-            console.log(data)
             this.setState(data)
         })
     }
@@ -22,6 +29,40 @@ export default class ProductShowPage extends Component {
             return { reviews: state.reviews.filter((review) => review.id !== reviewId) };
         });
     }
+    handleReviewForm(type, val) {
+        if (type === "CHANGE_BODY") {
+            const { body, ...rest } = this.state.review;
+            this.setState({
+                review: {
+                    body: val,
+                    ...rest
+                }
+            })
+        } else {
+            const { rating, ...rest } = this.state.review;
+            this.setState({
+                review: {
+                    rating: val,
+                    ...rest
+                }
+            })
+        }
+    }
+
+    handleAddNewReview(params) {
+        this.setState(
+            {
+                reviews: [
+                    ...this.state.reviews,
+                    {
+                        id: 123,
+                        ...params
+                    }
+                ]
+            }
+        )
+    }
+
     render() {
         return (
             <div>
@@ -33,6 +74,7 @@ export default class ProductShowPage extends Component {
                     price={this.state.price}
                 />
                 <ReviewList reviewList={this.state.reviews} onReviewDelete={this.deleteReview} />
+                <NewReviewForm onChange={this.handleReviewForm} review={this.state.review} handleAddNewReview={this.handleAddNewReview} />
             </div>
         )
     }
